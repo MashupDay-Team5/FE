fcdb2a37e59dda91ab4f9ff7103bc135570a17c0
j2nooh <wisestorm01@gmail.com>
2026-09-21 19:42:22 +0900

    chore: TypeScript strict와 경로 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // `@/`로 시작하는 import는 항상 `src` 폴더를 기준으로 한다.
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
